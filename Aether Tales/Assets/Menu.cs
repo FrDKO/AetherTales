472320133075c30709b856a597d4828584625412
Rico <RicoMRichardson@gmail.com>
2019-06-19 21:02:53 -0400

    Created base Card Creator

---
+++ Aether Tales/Assets/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Menu : MonoBehaviour
+{
+   List<GameObject> children;
+
+
+
+   public void NextMenu(Button b)
+   {
+       string key = b.name.Replace("Bttn","");
+       foreach(GameObject x in children)
+       {
+           if(x.tag.Equals(key))
+           {
+               x.SetActive(true);
+               x.GetComponent<MenuButton>();
+           }
+       }
+   }
+
+
+   public void Start()
+   {
+       getChildren();
+   }
+
+   public void getChildren()
+   {
+       for(int i = 0; i<this.transform.childCount;i++)
+       {
+           children.Add(this.transform.GetChild(i).gameObject);
+       }
+   }
+}
